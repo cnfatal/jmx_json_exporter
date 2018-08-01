a9fd5e5929eb3f6afb2d9b146ec3dcd475a9c06d
zhanglikun <zhanglikun@ghostcloud.cn>
2018-08-01 14:37:32 +0800

    对于值为数组的数据项，统计数组长度

--- collector/common_collector.go
+++ collector/common_collector.go
@@ -4,13 +4,13 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"strings"
 	"github.com/fatalc/jmx_json_exporter/utils"
-	)
+)
 
-const jmxEndpoint  = "/jmx"
+const jmxEndpoint = "/jmx"
 
 type CommonCollector struct {
-	hostname string
-	config map[string][]string
+	hostname   string
+	config     map[string][]string
 	Collectors map[string]prometheus.Collector
 }
 
@@ -23,20 +23,22 @@ func (bc *CommonCollector) Describe(ch chan<- *prometheus.Desc) {
 func (bc *CommonCollector) Collect(ch chan<- prometheus.Metric) {
 	beans := utils.JmxJsonBeansParse(utils.Get("http://" + bc.hostname + jmxEndpoint))
 	for k, v := range bc.Collectors {
-		vars := strings.Split(k,"#")
-		vGauge,ok:=v.(prometheus.Gauge)
+		vars := strings.Split(k, "#")
+		vGauge, ok := v.(prometheus.Gauge)
 		if ok {
-			data,ok := beans[vars[0]].Content[vars[1]].(float64)
-			if !ok {
-				continue
+			data := beans[vars[0]].Content[vars[1]]
+			switch data.(type) {
+			case float64:
+				vGauge.Set(data.(float64))
+			case []interface{}:
+				vGauge.Set(float64(len(data.([]interface{}))))
 			}
-			vGauge.Set(data)
 		}
 		v.Collect(ch)
 	}
 }
 
-func NewBeansCollector(host string,namespace string,config map[string][]string) *CommonCollector {
+func NewBeansCollector(host string, namespace string, config map[string][]string) *CommonCollector {
 	beansCollector := &CommonCollector{
 		hostname:   host,
 		config:     config,
@@ -44,10 +46,10 @@ func NewBeansCollector(host string,namespace string,config map[string][]string) 
 	}
 	for k, v := range config {
 		for _, v2 := range v {
-			beansCollector.Collectors[k + "#" +v2] = prometheus.NewGauge(
+			beansCollector.Collectors[k+"#"+v2] = prometheus.NewGauge(
 				prometheus.GaugeOpts{
 					Namespace:   namespace,
-					Subsystem:   strings.Split(k,":")[0],
+					Subsystem:   strings.Split(k, ":")[0],
 					Name:        v2,
 					Help:        "HelpOf" + v2,
 					ConstLabels: nil,
@@ -56,4 +58,3 @@ func NewBeansCollector(host string,namespace string,config map[string][]string) 
 	}
 	return beansCollector
 }
-
