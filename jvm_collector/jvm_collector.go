3c8e9538b9dfba9b279a1be043f940fb3601c38f
zhanglikun <zhanglikun@ghostcloud.cn>
2018-08-06 20:18:41 +0800

    Common Collector 新特性 增加带JVM数据的 NEW 操作

--- jvm_collector/jvm_collector.go
+++ jvm_collector/jvm_collector.go
@@ -9,7 +9,7 @@ const (
 	nameSpace = "JVM"
 )
 
-var config = Properties{
+var jvmConfig = Properties{
 	"java.lang:type=OperatingSystem": {
 		&PropertiesItem{"MaxFileDescriptorCount", TypeCounter, "最大文件描述合计"},
 		&PropertiesItem{"OpenFileDescriptorCount", TypeCounter, "打开文件描述合计"},
@@ -23,17 +23,42 @@ type JvmCollector interface {
 }
 
 type jvmCollector struct {
-	labels   map[string]string
-	config   Properties
-	hostPort string
-	*CommonCollector
+	labels          map[string]string
+	config          Properties
+	hostPort        string
+	commonCollector *CommonCollector
+	jvmCollector    *CommonCollector
+}
+
+func (jc *jvmCollector) Collect(ch chan<- Metric) {
+	jc.jvmCollector.Collect(ch)
+	if jc.commonCollector != nil {
+		jc.commonCollector.Collect(ch)
+	}
+}
+
+func (jc *jvmCollector) Describe(ch chan<- *Desc) {
+	jc.jvmCollector.Describe(ch)
+	if jc.commonCollector != nil {
+		jc.commonCollector.Describe(ch)
+	}
 }
 
 func NewJvmCollector(hostPort string) JvmCollector {
 	return &jvmCollector{
+		labels:       nil,
+		config:       jvmConfig,
+		hostPort:     hostPort,
+		jvmCollector: NewCommonCollector(hostPort, nameSpace, jvmConfig, nil),
+	}
+}
+
+func NewWithJvmCollector(hostPort string, namespace string, config Properties, labels map[string]string) JvmCollector {
+	return &jvmCollector{
 		labels:          nil,
 		config:          config,
 		hostPort:        hostPort,
-		CommonCollector: NewCommonCollector(hostPort,nameSpace,config,nil),
+		jvmCollector:    NewCommonCollector(hostPort, nameSpace, jvmConfig, nil),
+		commonCollector: NewCommonCollector(hostPort, namespace, config, labels),
 	}
 }
