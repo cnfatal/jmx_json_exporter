b84a7d3014fb590bf0b9ade30fe5f2434c297024
zhanglikun <zhanglikun@ghostcloud.cn>
2018-08-07 11:33:26 +0800

    hadoop exporter improved

--- hadoop_exporter/hadoop_exporter.go
+++ hadoop_exporter/hadoop_exporter.go
@@ -6,6 +6,10 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
+	"github.com/fatalc/jmx_json_exporter/utils"
+	"encoding/json"
+	"strings"
+	. "github.com/fatalc/jmx_json_exporter/collector"
 )
 
 var (
@@ -13,6 +17,83 @@ var (
 	port = flag.String("port", "8080", "The port of \"/metrics\"  output endpoint")
 	path = flag.String("path", "/metrics", "Path of output endpoint")
 )
+
+// 配置需要监控的数据项
+var hadoopConfig = Properties{
+	"hadoop": {
+		"Hadoop:service=NameNode,name=NameNodeInfo": {
+			&Property{"Total", TypeGauge, "磁盘总计",},
+			&Property{"Used", TypeGauge, "已使用",},
+			&Property{"Free", TypeGauge, "剩余",},
+			&Property{"NonDfsUsedSpace", TypeGauge, "非DFS使用空间",},
+		},
+		"Hadoop:service=DataNode,name=JvmMetrics": {
+			&Property{"MemNonHeapCommittedM", TypeGauge, "已使用内存",},
+			&Property{"LogWarn", TypeGauge, "日志警告",},
+		},
+	},
+}
+
+type HadoopCollector struct {
+	masterHosts map[string]string
+	nodeHosts   map[string]string
+	collectors  map[string]*CommonCollector
+}
+
+func (hc *HadoopCollector) Describe(ch chan<- *prometheus.Desc) {
+	for _, v := range hc.collectors {
+		v.Describe(ch)
+	}
+}
+
+//Collect implements the prometheus.Collector interface. 该接口调用来更新数据
+func (hc *HadoopCollector) Collect(ch chan<- prometheus.Metric) {
+	for _, v := range hc.collectors {
+		v.Collect(ch)
+	}
+}
+
+func NewHadoopCollector(masterHosts map[string]string) *HadoopCollector {
+	nodeHosts := getNodeHosts(masterHosts)
+	collectors := make(map[string]*CommonCollector, len(masterHosts))
+	for _, masterHostPort := range masterHosts {
+		collectors[masterHostPort] = NewCommonCollectorWithJvm(masterHostPort, hadoopConfig, nil)
+	}
+	for _, nodeHostPort := range nodeHosts {
+		collectors[nodeHostPort] = NewCommonCollectorWithJvm(nodeHostPort, hadoopConfig, nil)
+	}
+	return &HadoopCollector{
+		masterHosts: masterHosts,
+		nodeHosts:   nodeHosts,
+		collectors:  collectors,
+	}
+}
+
+func getNodeHosts(masterHosts map[string]string) map[string]string {
+	const protocol = "http://"
+	const nameNodeInfo = "Hadoop:service=NameNode,name=NameNodeInfo"
+	const path = "/jmx?qry=" + nameNodeInfo
+	const liveNodesName = "LiveNodes"
+	const infoKey = "infoAddr"
+
+	nodeUrls := make(map[string]string)
+	for _, v := range masterHosts {
+		beans, err := JmxJsonBeansParse(utils.Get(protocol + v + path))
+		if err != nil {
+			log.Printf("%s 下未找到 Datanode 或地址无法访问")
+			continue
+		}
+		liveNodes := beans[nameNodeInfo].Content[liveNodesName].(string)
+		nodesJson := make(map[string]interface{})
+		json.Unmarshal([]byte(strings.Trim(liveNodes, "/")), &nodesJson)
+		for host, v := range nodesJson {
+			nodeUrl := v.(map[string]interface{})[infoKey].(string)
+			host = strings.Split(host, ":")[0] + ":" + strings.Split(nodeUrl, ":")[1]
+			nodeUrls[host] = host
+		}
+	}
+	return nodeUrls
+}
 
 func init() {
 	log.Printf("initalizing")
